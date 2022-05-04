bff45d368edff780e6ba3646388c017e641c381c
Stefan Prugg <stefan.prugg@prugg.at>
2022-05-04 20:41:08 +0200

    added data driven tests for MsTest

---
+++ MSTestAndFluentTests/DataDriven/DataDrivenDataRowsExamples.cs
@@ -0,0 +1,94 @@
+﻿using BusinessLogic.PurchaseOrderModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MSTestAndFluentTests.DataDriven
+{
+    /// <summary>
+    /// Demos data driven tests using DataRow attribute
+    /// </summary>
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class DataDrivenDataRowsExamples
+    {
+        private PurchaseOrder _purchaseOrder;        
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _purchaseOrder = new PurchaseOrderFactory().CreatePurchaseOrderInstance();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // NOP
+        }
+
+        [DataTestMethod]
+        [DataRow("1", "!§$%&/(")]
+        [DataRow("2", "/*-+")]
+        public void Given_Special_Characters_Expect_Corrent_ItemName(string itemId, string itemName )
+        {
+            // act
+            PurchaseItem purchaseItem = new PurchaseItem()
+            {
+                ItemId = itemId,
+                ItemName = itemName,
+                Quantity = 1,
+                Price = 1.0M
+            };
+
+            // assert
+            Assert.AreEqual(itemName, purchaseItem.ItemName);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow(null)]
+        [DataRow(" ")]
+        [DataRow("     ")]
+        public void Given_EmptyNullOrWhiteSpace_ItemId_Expect_Guid(string itemId)
+        {
+            // arrange
+
+            // act
+            PurchaseItem purchaseItem = new PurchaseItem()
+            {
+                ItemId = itemId,
+                ItemName = "Testing item with null or empty values",
+                Quantity = 2,
+                Price = 123.45M
+            };
+
+            // assert
+            Assert.IsTrue(Guid.TryParse(purchaseItem.ItemId, out Guid guidResult));
+        }
+
+        /// <summary>
+        /// Demos that you cannot pass a decimal value in a DataRow attribute. This is a limitation of C# and not
+        /// a limitation of the test framework.
+        /// 
+        /// Cannot compile due to:
+        /// An attribute argument must be a constant expression, typeof expression or array 
+        /// creation expression of an attribute parameter type. Compiler Error CS0182
+        ///         
+        /// Instead, <use cref="DataDrivenDynamicDataExamples"/>
+        /// </summary>     
+        // [DataTestMethod]
+        // [DataRow(164.80M)]
+        public void Cannot_pass_decimal_value(decimal itemPrice)
+        {
+            PurchaseItem purchaseItem = new PurchaseItem()
+            {
+                ItemId = Guid.NewGuid().ToString(),
+                ItemName = "Test Item",
+                Quantity = 12,
+                Price = itemPrice
+            };
+
+            Assert.AreEqual(itemPrice, purchaseItem.Price);
+        }
+    }
+}
---
+++ MSTestAndFluentTests/DataDriven/DataDrivenDynamicDataExamples.cs
@@ -0,0 +1,100 @@
+﻿using BusinessLogic.PurchaseOrderModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedForTests.PurchaseOrderModule;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSTestAndFluentTests.DataDriven
+{
+    /// <summary>
+    /// Demos data driven tests using DynamicData attribute
+    /// </summary>
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class DataDrivenDynamicDataExamples
+    {
+        private PurchaseOrder _purchaseOrder;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _purchaseOrder = new PurchaseOrderFactory().CreatePurchaseOrderInstance();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // NOP
+        }
+
+        /// <summary>
+        /// Test using DynamicData for data driven tests. The data source must return IEnumerable<object[]>.
+        /// </summary>
+        /// <param name="purchaseItem"></param>
+        [DataTestMethod]
+        [DynamicData(nameof(ItemsTestDataAsMethod), DynamicDataSourceType.Method)]
+        public void DynamicDataAsMethod_Adding_Item_Should_Update_TotalAmount(PurchaseItem purchaseItem, decimal exptectedTotalAmount)
+        {
+            // act
+            _purchaseOrder.AddPurchaseItem(purchaseItem);
+
+            // assert
+            Assert.AreEqual(exptectedTotalAmount, _purchaseOrder.TotalAmount);
+        }
+
+        /// <summary>
+        /// Returns items test data via a method.
+        /// </summary>
+        /// <returns></returns>
+        protected static IEnumerable<object[]> ItemsTestDataAsMethod()
+        {
+            yield return new object[]
+                {
+                    new PurchaseItem() { ItemId = Guid.NewGuid().ToString(), ItemName = "TestItem 42 from method", Quantity = 10, Price = 64.95M },
+                    649.5M
+                };
+
+            yield return new object[]
+                {
+                    new PurchaseItem() { ItemId = Guid.NewGuid().ToString(), ItemName = "TestItem 43 from method", Quantity = 4, Price = 11.01M },
+                    44.04M
+                };
+        }
+
+        /// <summary>
+        /// Test using DynamicData for data driven tests. The data source must return IEnumerable<object[]>.
+        /// </summary>
+        /// <param name="purchaseItem"></param>
+        [DataTestMethod]
+        [DynamicData(nameof(ItemsTestDataAsProperty), DynamicDataSourceType.Property)]
+        public void DynamicDataAsProperty_Adding_Item_Should_Update_TotalAmount(PurchaseItem purchaseItem)
+        {
+            // act
+            _purchaseOrder.AddPurchaseItem(purchaseItem);
+
+            // assert
+            Assert.AreEqual(purchaseItem.Price * purchaseItem.Quantity, _purchaseOrder.TotalAmount);
+        }
+
+        /// <summary>
+        /// Returns items test data via a property.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<object[]> ItemsTestDataAsProperty
+        {
+            get
+            {
+                yield return new object[]
+                    {
+                    new PurchaseItem() { ItemId = Guid.NewGuid().ToString(), ItemName = "TestItem 42 from property", Quantity = 10, Price = 64.95M }
+                    };
+
+                yield return new object[]
+                    {
+                    new PurchaseItem() { ItemId = Guid.NewGuid().ToString(), ItemName = "TestItem 43 from property", Quantity = 4, Price = 11.01M }
+                    };
+            }
+        }
+    }
+}
+
